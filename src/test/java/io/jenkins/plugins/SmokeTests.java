7b72eeb50fd68b935364662ee4c9add9b1c8765d
Valentin Delaye <jonesbusy@gmail.com>
2024-10-19 14:40:02 +0200

    Replace test with RealJenkinsRule

--- src/test/java/io/jenkins/plugins/SmokeTests.java
+++ src/test/java/io/jenkins/plugins/SmokeTests.java
@@ -1,45 +1,42 @@
 package io.jenkins.plugins;
 
+import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.client.WireMock;
-import com.github.tomakehurst.wiremock.junit5.WireMockRuntimeInfo;
-import com.github.tomakehurst.wiremock.junit5.WireMockTest;
 import java.io.File;
 import java.nio.file.Files;
-import org.jfrog.artifactory.client.Artifactory;
-import org.jfrog.artifactory.client.ArtifactoryClientBuilder;
-import org.jfrog.artifactory.client.UploadableArtifact;
-import org.junit.jupiter.api.Test;
+import java.util.logging.Level;
+import org.junit.Rule;
+import org.junit.Test;
+import org.jvnet.hudson.test.JenkinsRule;
+import org.jvnet.hudson.test.RealJenkinsRule;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@WireMockTest
 public class SmokeTests {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SmokeTests.class);
 
-    @Test
-    public void smokeTest(WireMockRuntimeInfo wmRuntimeInfo) throws Exception {
+    @Rule
+    public RealJenkinsRule jj = new RealJenkinsRule().withLogger(SmokeTests.class, Level.FINEST);
 
-        // WireMock stub
-        WireMock wireMock = wmRuntimeInfo.getWireMock();
+    @Test
+    public void test() throws Throwable {
+        jj.startJenkins();
+        jj.runRemotely(SmokeTests::smokeTest);
+    }
+
+    public static void smokeTest(JenkinsRule r) throws Throwable {
+
+        WireMockServer wireMock = new WireMockServer(8181);
+        wireMock.start();
 
         File testFile = Files.createTempFile("text", ".txt").toFile();
 
         // PUT to upload artifact
-        wireMock.register(
+        wireMock.stubFor(
                 WireMock.put(WireMock.urlMatching("/my-generic-repo/.*")).willReturn(WireMock.okJson("{}")));
 
-        try (Artifactory artifactory = ArtifactoryClientBuilder.create()
-                .setUrl(wmRuntimeInfo.getHttpBaseUrl())
-                .setUsername("fake")
-                .setPassword("fake")
-                .build()) {
-            UploadableArtifact artifact =
-                    artifactory.repository("my-generic-repo").upload("text.txt", testFile);
-            artifact.withSize(Files.size(testFile.toPath()));
-            artifact.withListener(
-                    (bytesRead, totalBytes) -> LOGGER.info(String.format("Uploaded %d/%d", bytesRead, totalBytes)));
-            artifact.doUpload();
-        }
+        // Test upload
+        TestUtils.testUpload("my-generic-repo", wireMock.baseUrl(), testFile);
     }
 }
