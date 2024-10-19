927b68f2c961689c8de228c46cca65cee88cb26a
Valentin Delaye <jonesbusy@gmail.com>
2024-10-19 14:42:06 +0200

    Replace with real jenkins rule test

--- src/test/java/io/jenkins/plugins/SmokeTests.java
+++ src/test/java/io/jenkins/plugins/SmokeTests.java
@@ -5,6 +5,9 @@ import com.github.tomakehurst.wiremock.client.WireMock;
 import java.io.File;
 import java.nio.file.Files;
 import java.util.logging.Level;
+import org.jfrog.artifactory.client.Artifactory;
+import org.jfrog.artifactory.client.ArtifactoryClientBuilder;
+import org.jfrog.artifactory.client.UploadableArtifact;
 import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
@@ -20,12 +23,17 @@ public class SmokeTests {
     public RealJenkinsRule jj = new RealJenkinsRule().withLogger(SmokeTests.class, Level.FINEST);
 
     @Test
-    public void test() throws Throwable {
+    public void smokeUploadTest() throws Throwable {
         jj.startJenkins();
         jj.runRemotely(SmokeTests::smokeTest);
     }
 
-    public static void smokeTest(JenkinsRule r) throws Throwable {
+    /**
+     * Smoke test for the plugin
+     * @param r the Jenkins rule
+     * @throws Exception if the test fails
+     */
+    public static void smokeTest(JenkinsRule r) throws Exception {
 
         WireMockServer wireMock = new WireMockServer(8181);
         wireMock.start();
@@ -37,6 +45,26 @@ public class SmokeTests {
                 WireMock.put(WireMock.urlMatching("/my-generic-repo/.*")).willReturn(WireMock.okJson("{}")));
 
         // Test upload
-        TestUtils.testUpload("my-generic-repo", wireMock.baseUrl(), testFile);
+        testUpload("my-generic-repo", wireMock.baseUrl(), testFile);
+    }
+
+    /**
+     * Test uploading a file to an Artifactory server
+     * @param url the URL of the Artifactory server
+     * @param testFile the file to upload
+     * @throws Exception if the file cannot be uploaded
+     */
+    private static void testUpload(String repo, String url, File testFile) throws Exception {
+        try (Artifactory artifactory = ArtifactoryClientBuilder.create()
+                .setUrl(url)
+                .setUsername("fake")
+                .setPassword("fake")
+                .build()) {
+            UploadableArtifact artifact = artifactory.repository(repo).upload("text.txt", testFile);
+            artifact.withSize(Files.size(testFile.toPath()));
+            artifact.withListener(
+                    (bytesRead, totalBytes) -> LOGGER.info(String.format("Uploaded %d/%d", bytesRead, totalBytes)));
+            artifact.doUpload();
+        }
     }
 }
